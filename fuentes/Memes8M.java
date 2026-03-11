5f3292112f2dee91a7bbaa2490724482ee0b648f
Miguel Jaque <mjaque@migueljaque.com>
2026-03-11 08:16:12 +0100

    Versión Inicial.

---
+++ fuentes/Memes8M.java
@@ -0,0 +1,31 @@
+/**
+	Fichero con la clase principal del programa
+
+	@author Miguel Jaque Barbero
+	@date 8 de marzo de 2026.
+**/
+
+
+/**
+	Clase principal del programa.
+**/
+public class Memes8M{
+
+	/**
+		Método inicial del programa.
+
+		@param args Array de argumentos de la línea de comando. No se utilizan.
+	**/
+	public static void main(String[] args){
+		presentar();
+	}
+
+	/**
+		Presenta el título y las instrucciones del programa.
+	**/
+	public static void presentar(){
+		System.out.println("-- Juego de MEMES del 8 de Marzo -- ");
+		System.out.println();
+		System.out.println("¿Te dejas engañar por los memes?");
+	}
+}
---
+++ tests/Memes8MTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+class Memes8MTests {
+    
+	@Test
+    @DisplayName("Test constructor válido")
+    public void testConstructorValido() {
+		Memes8M programa = new Memes8M();
+		assertNotNull(programa, "El constructor devuelve null.");
+	}
+
+	@Test
+	@DisplayName("Test se muestra título")
+	public void testMuestraTitulo() {
+		PrintStream salidaConsola = System.out;
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream salidaTest = new PrintStream(baos);
+		System.setOut(salidaTest);
+		try{
+			Memes8M.main(null);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			fail("Excepción inesperada.");
+		}
+		System.setOut(salidaConsola);
+		String salida = baos.toString();
+		assertTrue(salida.contains("Juego de MEMES del 8 de Marzo"), "La salida no contiene el título esperado.");
+	}
+}
